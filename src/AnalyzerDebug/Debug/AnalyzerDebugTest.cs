99a64015a70a89c9a684e04e7da233062007954b
djfr <davidferreirarodrigues@gmail.com>
2016-08-30 11:40:06 +0100

    Added an analyzer debug solution to the repo.

---
+++ src/AnalyzerDebug/Debug/AnalyzerDebugTest.cs
@@ -0,0 +1,18 @@
+﻿namespace Debug
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class AnalyzerDebugTest
+    {
+        [TestMethod]
+        public void DebugMethod1()
+        {
+        }
+
+        [TestMethod]
+        public void DebugMethod2()
+        {
+        }
+    }
+}
