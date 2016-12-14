f7ba66ce4b249183e009702a4bc32e55013555dc
djfr <davidferreirarodrigues@gmail.com>
2016-12-14 10:17:20 +0000

    Bumped framework version to 4.6.2 and realigned all packages

---
+++ src/Tests/DevOpsFlex.Tests.Core.Tests/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsFlex.Tests.Core.Tests
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
