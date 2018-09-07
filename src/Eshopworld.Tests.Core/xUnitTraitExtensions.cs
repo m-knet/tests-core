64609fb7f1306a11707b8f6bc3fdd1585b9b289e
djfr <davidferreirarodrigues@gmail.com>
2018-09-07 16:20:01 +0100

    current debug state

--- src/Eshopworld.Tests.Core/xUnitTraitExtensions.cs
+++ src/Eshopworld.Tests.Core/xUnitTraitExtensions.cs
@@ -6,204 +6,211 @@
     using Xunit.Abstractions;
     using Xunit.Sdk;
 
-    /// <summary>
-    /// Decorates a test as a Unit Test, so that it runs in Continuous Integration builds.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsUnitAttribute : EswCategoryAttribute
+    ///// <summary>
+    ///// Decorates a test as a Unit Test, so that it runs in Continuous Integration builds.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsUnitAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsUnitAttribute"/>
+    //    /// </summary>
+    //    public IsUnitAttribute() : base("Unit") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as an Integration Test, so that it runs in Continuous Integration builds.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsIntegrationAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsIntegrationAttribute"/>
+    //    /// </summary>
+    //    public IsIntegrationAttribute() : base("Integration") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as a Read Only Integration Test, so that it runs in Continuous Integration builds and can also run in Production.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsIntegrationReadOnlyAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsIntegrationReadOnlyAttribute"/>
+    //    /// </summary>
+    //    public IsIntegrationReadOnlyAttribute() : base("Integration", "ReadOnly") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as a WarmUp Test, so that it runs in Continuous Integration builds ahead of Integration tests
+    /////     to warm up the deployment and kick off things like EF migrations.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsWarmUpAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsWarmUpAttribute"/>
+    //    /// </summary>
+    //    public IsWarmUpAttribute() : base("WarmUp") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as a Coded UI Test, so that it runs in Continuous Integration builds as a Coded UI test.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsAutomatedUiAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsAutomatedUiAttribute"/>
+    //    /// </summary>
+    //    public IsAutomatedUiAttribute() : base("AutomatedUi") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as a Unit Test and as requiring Fakes to be present, so that it runs in Continuous Integration builds.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsFakesAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsFakesAttribute"/>
+    //    /// </summary>
+    //    public IsFakesAttribute() : base("Unit", "Fakes") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as a Unit Test and as requiring Code Contracts to be present, so that it runs in Continuous Integration builds.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsCodeContractAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsCodeContractAttribute"/>
+    //    /// </summary>
+    //    public IsCodeContractAttribute() : base("Unit", "CodeContract") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as a Unit Test and as requiring Roslyn to be present, so that it runs in Continuous Integration builds.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsRoslynAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsRoslynAttribute"/>
+    //    /// </summary>
+    //    public IsRoslynAttribute() : base("Unit", "Roslyn") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as a development entry point.
+    ///// This test never runs on any automated build, is purely a development facility.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsDevAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsDevAttribute"/>
+    //    /// </summary>
+    //    public IsDevAttribute() : base("Dev") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as profiler entry point for profiling CPU.
+    ///// This test never runs on any automated build, is purely a development facility.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsProfilerCpuAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsProfilerCpuAttribute"/>
+    //    /// </summary>
+    //    public IsProfilerCpuAttribute() : base("Profiler CPU") { }
+    //}
+
+    ///// <summary>
+    ///// Decorates a test as profiler entry point for profiling memory.
+    ///// This test never runs on any automated build, is purely a development facility.
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsProfilerMemoryAttribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsProfilerMemoryAttribute"/>
+    //    /// </summary>
+    //    public IsProfilerMemoryAttribute() : base("Profiler Memory") { }
+    //}
+
+    ///// <summary>
+    ///// decorates a test as L0 test
+    ///// this was added to facilitate lowest level of L(x) test categorization, logically equivalent to basic unit test
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsLayer0Attribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsProfilerMemoryAttribute"/>
+    //    /// </summary>
+    //    public IsLayer0Attribute() : base("Layer0") { }
+    //}
+
+    ///// <summary>
+    ///// L1 test aimed at being executed during build stage - effectively CI attribute
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsLayer1Attribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsProfilerMemoryAttribute"/>
+    //    /// </summary>
+    //    public IsLayer1Attribute() : base("Layer1") { }
+    //}
+
+    ///// <summary>
+    ///// L1 test aimed at being executed during deploy stage - effectively CD attribute
+    ///// </summary>
+    //[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
+    //public sealed class IsLayer2Attribute : EswCategoryAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="IsProfilerMemoryAttribute"/>
+    //    /// </summary>
+    //    public IsLayer2Attribute() : base("Layer2") { }
+    //}
+
+    ///// <summary>
+    ///// Base class for all the category model attributes. Contains a list of all the attribute categories
+    ///// and supports multiple categories.
+    ///// </summary>
+    //[TraitDiscoverer(EswCategoryDiscoverer.FullyQualifiedName, EswCategoryDiscoverer.Namespace)]
+    //public abstract class EswCategoryAttribute : Attribute, ITraitAttribute
+    //{
+    //    /// <summary>
+    //    /// Initializes a new instance of <see cref="EswCategoryAttribute"/>.
+    //    /// </summary>
+    //    /// <param name="categories">The trait categories present in the attribute.</param>
+    //    protected EswCategoryAttribute(params string[] categories)
+    //    {
+    //        Categories = categories.ToList();
+    //    }
+
+    //    /// <summary>
+    //    /// Gets the list of categories in the test that the attribute is decorating.
+    //    /// Exposed as public because xUnit needs to reflect the getter to get to it.
+    //    /// </summary>
+    //    public IEnumerable<string> Categories { get; }
+    //}
+
+
+    [TraitDiscoverer("Eshopworld.Tests.Core.EswCategoryDiscoverer", "Eshopworld.Tests.Core")]
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
+    public class FooTestAttribute : Attribute, ITraitAttribute
     {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsUnitAttribute"/>
-        /// </summary>
-        public IsUnitAttribute() : base("Unit") { }
     }
 
     /// <summary>
-    /// Decorates a test as an Integration Test, so that it runs in Continuous Integration builds.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsIntegrationAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsIntegrationAttribute"/>
-        /// </summary>
-        public IsIntegrationAttribute() : base("Integration") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as a Read Only Integration Test, so that it runs in Continuous Integration builds and can also run in Production.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsIntegrationReadOnlyAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsIntegrationReadOnlyAttribute"/>
-        /// </summary>
-        public IsIntegrationReadOnlyAttribute() : base("Integration", "ReadOnly") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as a WarmUp Test, so that it runs in Continuous Integration builds ahead of Integration tests
-    ///     to warm up the deployment and kick off things like EF migrations.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsWarmUpAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsWarmUpAttribute"/>
-        /// </summary>
-        public IsWarmUpAttribute() : base("WarmUp") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as a Coded UI Test, so that it runs in Continuous Integration builds as a Coded UI test.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsAutomatedUiAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsAutomatedUiAttribute"/>
-        /// </summary>
-        public IsAutomatedUiAttribute() : base("AutomatedUi") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as a Unit Test and as requiring Fakes to be present, so that it runs in Continuous Integration builds.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsFakesAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsFakesAttribute"/>
-        /// </summary>
-        public IsFakesAttribute() : base("Unit", "Fakes") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as a Unit Test and as requiring Code Contracts to be present, so that it runs in Continuous Integration builds.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsCodeContractAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsCodeContractAttribute"/>
-        /// </summary>
-        public IsCodeContractAttribute() : base("Unit", "CodeContract") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as a Unit Test and as requiring Roslyn to be present, so that it runs in Continuous Integration builds.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsRoslynAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsRoslynAttribute"/>
-        /// </summary>
-        public IsRoslynAttribute() : base("Unit", "Roslyn") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as a development entry point.
-    /// This test never runs on any automated build, is purely a development facility.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsDevAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsDevAttribute"/>
-        /// </summary>
-        public IsDevAttribute() : base("Dev") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as profiler entry point for profiling CPU.
-    /// This test never runs on any automated build, is purely a development facility.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsProfilerCpuAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsProfilerCpuAttribute"/>
-        /// </summary>
-        public IsProfilerCpuAttribute() : base("Profiler CPU") { }
-    }
-
-    /// <summary>
-    /// Decorates a test as profiler entry point for profiling memory.
-    /// This test never runs on any automated build, is purely a development facility.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsProfilerMemoryAttribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsProfilerMemoryAttribute"/>
-        /// </summary>
-        public IsProfilerMemoryAttribute() : base("Profiler Memory") { }
-    }
-
-    /// <summary>
-    /// decorates a test as L0 test
-    /// this was added to facilitate lowest level of L(x) test categorization, logically equivalent to basic unit test
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsLayer0Attribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsProfilerMemoryAttribute"/>
-        /// </summary>
-        public IsLayer0Attribute() : base("Layer0") { }
-    }
-
-    /// <summary>
-    /// L1 test aimed at being executed during build stage - effectively CI attribute
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsLayer1Attribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsProfilerMemoryAttribute"/>
-        /// </summary>
-        public IsLayer1Attribute() : base("Layer1") { }
-    }
-
-    /// <summary>
-    /// L1 test aimed at being executed during deploy stage - effectively CD attribute
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method)]
-    public sealed class IsLayer2Attribute : EswCategoryAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="IsProfilerMemoryAttribute"/>
-        /// </summary>
-        public IsLayer2Attribute() : base("Layer2") { }
-    }
-
-    /// <summary>
-    /// Base class for all the category model attributes. Contains a list of all the attribute categories
-    /// and supports multiple categories.
-    /// </summary>
-    [TraitDiscoverer(EswCategoryDiscoverer.FullyQualifiedName, EswCategoryDiscoverer.Namespace)]
-    public abstract class EswCategoryAttribute : Attribute, ITraitAttribute
-    {
-        /// <summary>
-        /// Initializes a new instance of <see cref="EswCategoryAttribute"/>.
-        /// </summary>
-        /// <param name="categories">The trait categories present in the attribute.</param>
-        protected EswCategoryAttribute(params string[] categories)
-        {
-            Categories = categories;
-        }
-
-        /// <summary>
-        /// Gets the list of categores in the test that the attribute is decorating.
-        /// Exposed as public because xUnit needs to reflect the getter to get to it.
-        /// </summary>
-        public IEnumerable<string> Categories { get; }
-    }
-
-    /// <summary>
-    /// Discoverer that provides eShopWorld trait values to xUnit.net v2 tests.
+    /// Discoverer that provides eShopWorld trait values to xUnit.net tests.
     /// </summary>
     public class EswCategoryDiscoverer : ITraitDiscoverer
     {
@@ -215,8 +222,10 @@
         /// <returns>The trait values.</returns>
         public IEnumerable<KeyValuePair<string, string>> GetTraits(IAttributeInfo traitAttribute)
         {
-            var categories = traitAttribute.GetNamedArgument<IEnumerable<string>>(nameof(EswCategoryAttribute.Categories));
-            return categories.Select(c => new KeyValuePair<string, string>("Category", c));
+            yield return new KeyValuePair<string, string>("Category", "crapzor");
+
+            //var categories = ((traitAttribute as ReflectionAttributeInfo)?.Attribute as EswCategoryAttribute)?.Categories;
+            //return categories?.Select(c => new KeyValuePair<string, string>("Category", c)) ?? new[] {new KeyValuePair<string, string>("Category", "ERROR")};
         }
     }
 }
---
+++ src/Tests/Eshopworld.Tests.Core.Tests2/XunitTraitExtensionsTest.cs
@@ -0,0 +1,46 @@
+﻿namespace Eshopworld.Tests.Core.Tests
+{
+    using Xunit;
+
+    public class XunitTraitExtensionsTest
+    {
+        [Fact, IsUnit]
+        public void Test_IsUnit() { }
+
+        [Fact, IsIntegration]
+        public void Test_IsIntegration() { }
+
+        [Fact, IsFakes]
+        public void Test_IsFakes() { }
+
+        [Fact, IsCodeContract]
+        public void Test_IsCodeContract() { }
+
+        [Fact, IsRoslyn]
+        public void Test_IsRoslyn() { }
+
+        [Fact, IsDev]
+        public void Test_IsIsDev() { }
+
+        [Fact, IsProfilerCpu]
+        public void Test_IsProfilerCpu() { }
+
+        [Fact, IsProfilerMemory]
+        public void Test_IsProfilerMemory() { }
+
+        [Fact, IsLayer0]
+        public void Test_IsLayer0() { }
+
+        [Fact, IsLayer1]
+        public void Test_IsLayer1() { }
+
+        [Fact, IsLayer2]
+        public void Test_IsLayer2() { }
+
+        [Fact, Trait("Category", "Unit")]
+        public void Foo() { }
+
+        [Fact, FooTest]
+        public void Foo2() { }
+    }
+}
