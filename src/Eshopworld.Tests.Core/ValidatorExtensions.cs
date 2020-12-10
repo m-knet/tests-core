f466bb803dbd1619bcce15724b34d1351395cddb
Victor Palomares <vpalomares@eshopworld.com>
2020-12-10 15:43:47 +0100

    documentation updated

--- src/Eshopworld.Tests.Core/ValidatorExtensions.cs
+++ src/Eshopworld.Tests.Core/ValidatorExtensions.cs
@@ -15,9 +15,11 @@ namespace Eshopworld.Tests.Core
         /// </summary>
         /// <typeparam name="T"></typeparam>
         /// <param name="mock"></param>
-        /// <param name="errorMessage"></param>
+        /// <param name="errorMessage">When an error is specified then validation will fail</param>
+        /// <param name="propertyName">Used only when <paramref name="errorMessage"/> is specified.
+        /// When null a random value is assigned to the property name validation error </param>
         /// <returns></returns>
-        public static Mock<IValidator<T>> MockValidateAsync<T> (this Mock<IValidator<T>> mock, string propertyName =null, string errorMessage = null)
+        public static Mock<IValidator<T>> MockValidateAsync<T> (this Mock<IValidator<T>> mock, string errorMessage = null, string propertyName =null)
         {
             var result = new ValidationResult();
             if (!string.IsNullOrEmpty(errorMessage))
